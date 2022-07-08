6da00c54b21481e5f6dd8dbd97a5b4f0a53c6ee4
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2022-07-08 09:07:49 +0700

    change logo

--- templates/installation.php
+++ templates/installation.php
@@ -4,7 +4,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<title>Installasi</title>
 	<meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
-	<link rel="icon" href="assets/img/main-logo.png" type="image/x-icon"/>
+	<link rel="icon" href="assets/img/main-logo.gif" type="image/x-icon"/>
 
 	<!-- Fonts and icons -->
 	<script src="assets/js/plugin/webfont/webfont.min.js"></script>
@@ -29,7 +29,7 @@
                 <div class="card full-height">
                     <div class="card-body">
                         <center>
-                            <img src="assets/img/main-logo.png" width="150px" height="100px" alt="logo" style="object-fit:contain;">
+                            <img src="assets/img/main-logo.gif" width="150px" height="100px" alt="logo" style="object-fit:contain;">
                         </center>
                         <div class="card-title text-center">Form Instalasi</div>
 
