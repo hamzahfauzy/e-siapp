e5962cd115f7faad06b8f001bf308d2c1d0c7032
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2022-07-26 10:30:03 +0700

    update opd

--- actions/kegiatan/cetak.php
+++ actions/kegiatan/cetak.php
@@ -7,35 +7,29 @@ $db   = new Database($conn);
 $success_msg = get_flash_msg('success');
 
 $data = $db->all($table);
-$data = array_map(function($d){
-    if($d->opd_2021)
+$data = array_map(function($d) use ($db){
+    $opds = [];
+    $opds[] = ['no'=>1,'opd'=>$d->opd_2021,'tahun'=>2021];
+    $opds[] = ['no'=>2,'opd'=>$d->opd_2022,'tahun'=>2022];
+    $opds[] = ['no'=>3,'opd'=>$d->opd_2023,'tahun'=>2023];
+    $opds[] = ['no'=>4,'opd'=>$d->opd_2024,'tahun'=>2024];
+    $opds[] = ['no'=>5,'opd'=>$d->opd_2025,'tahun'=>2025];
+    $opds[] = ['no'=>6,'opd'=>$d->opd_2026,'tahun'=>2026];
+
+    if(count(array_unique($opds)) > 1)
     {
-        $d->opd = $d->opd_2021;
+        $opds = array_map(function($opd, $index) use ($db){
+            $nm_opd = $db->single('opd',['kd_opd'=>$opd['opd']])->nm_opd;
+            return $opd['no'].'. '.$nm_opd.' ('.$opd['tahun'].')';
+        }, $opds);
+
+        $d->opd = implode('<br>',$opds);
     }
-    else if($d->opd_2022)
+    else
     {
-        $d->opd = $d->opd_2022;
+        $d->opd = $opds[0];
     }
-    else if($d->opd_2023)
-    {
-        $d->opd = $d->opd_2023;
-    }
-    else if($d->opd_2023)
-    {
-        $d->opd = $d->opd_2023;
-    }
-    else if($d->opd_2024)
-    {
-        $d->opd = $d->opd_2024;
-    }
-    else if($d->opd_2025)
-    {
-        $d->opd = $d->opd_2025;
-    }
-    else if($d->opd_2026)
-    {
-        $d->opd = $d->opd_2026;
-    }
+
     return $d;
 }, $data);
 
@@ -79,7 +73,7 @@ $fields = [
     ],
     'opd'  => [
         'label' => 'OPD Penanggung Jawab',
-        'type'  => 'options-obj:opd,kd_opd,nm_opd'
+        'type'  => 'text'
     ],
 ];
 
