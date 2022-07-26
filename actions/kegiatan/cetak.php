c258b3bfeace89b497603384460d9008f887f0a1
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2022-07-26 09:24:18 +0700

    opd pada kegiatan

--- actions/kegiatan/cetak.php
+++ actions/kegiatan/cetak.php
@@ -7,6 +7,37 @@ $db   = new Database($conn);
 $success_msg = get_flash_msg('success');
 
 $data = $db->all($table);
+$data = array_map(function($d){
+    if($d->opd_2021)
+    {
+        $d->opd = $d->opd_2021;
+    }
+    else if($d->opd_2022)
+    {
+        $d->opd = $d->opd_2022;
+    }
+    else if($d->opd_2023)
+    {
+        $d->opd = $d->opd_2023;
+    }
+    else if($d->opd_2023)
+    {
+        $d->opd = $d->opd_2023;
+    }
+    else if($d->opd_2024)
+    {
+        $d->opd = $d->opd_2024;
+    }
+    else if($d->opd_2025)
+    {
+        $d->opd = $d->opd_2025;
+    }
+    else if($d->opd_2026)
+    {
+        $d->opd = $d->opd_2026;
+    }
+    return $d;
+}, $data);
 
 $fields = [
     'kd_prioritas' => [
@@ -46,7 +77,7 @@ $fields = [
         'label' => '2026',
         'type'  => 'text'
     ],
-    'opd_2021'  => [
+    'opd'  => [
         'label' => 'OPD Penanggung Jawab',
         'type'  => 'options-obj:opd,kd_opd,nm_opd'
     ],
