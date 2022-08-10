3654ffcbed7f302be1599d3925994092a7c0efea
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2022-08-11 00:43:02 +0700

    ok

--- actions/reports/capaian.php
+++ actions/reports/capaian.php
@@ -61,8 +61,6 @@ if(isset($_GET['filter']['tahun']))
           ]);
           $cnt = count($cnt);
 
-          echo $cnt;
-
           foreach(['2021','2022','2023','2024','2025','2026'] as $thn)
           {
 
